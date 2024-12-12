d8e3bb06d23db143885ac47bc1c18714f0adab6b
unknown <joph941@gmail.com>
2024-12-12 12:53:11 +0530

    my cypress 1

---
+++ cypress/e2e/tests/olx.cy.js
@@ -0,0 +1,8 @@
+describe('User Registration Flow', () => {
+    it('should allow a user to sign up with valid details', () => {
+      cy.visit('https://www.flipkart.com/');
+   
+    });
+  
+    
+  });
